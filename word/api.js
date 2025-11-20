95d03dd9e110c2c3c56e011e8cc1db26b0b9a960
tcb602201731 <602201731@qq.com>
2025-11-20 11:09:28 +0800

    no message

--- word/api.js
+++ word/api.js
@@ -9359,33 +9359,18 @@ background-repeat: no-repeat;\
 			const oApi = this;
 			var loadImageCallBack = function (loadedImage) {
 				oLogicDocument.StartAction();
-				console.log("🚀 ~ loadImageCallBack ~ loadedImage:", loadedImage)
 				if (loadedImage.Image) {
 					let oDrawing = new AscCommonWord.ParaDrawing(width, height, null, oApi.WordControl.m_oDrawingDocument, oLogicDocument, null);
 					let oImage = oLogicDocument.DrawingObjects.createImage(loadedImage.src, 0, 0, width, height);
 					oImage.setParent(oDrawing);
 					oDrawing.Set_GraphicObject(oImage);
-					console.log("🚀 ~ oDrawing:", oDrawing)
 
 					var oImageProps = new asc_CImgProperty();
-					console.log("🚀 ~ oImageProps:", oImageProps)
 					// 选择一种环绕方式
 					// oImageProps.asc_putWrappingStyle(c_oAscWrapStyle2.Behind);
-					console.log("🚀 ~ oImageProps:11111111111")
 					// oImageProps.asc_putWrappingStyle(c_oAscWrapStyle2.InFront);
 					oImageProps.asc_putWrappingStyle(c_oAscWrapStyle2.Inline);
 					oDrawing.Set_Props(oImageProps);
-					console.log("🚀 ~ oImageProps:2222222222222")
-					// oDrawing.Set_DrawingType(drawing_Anchor);
-					// console.log("🚀 ~ oImage:2")
-					// oDrawing.Set_WrappingType(WRAPPING_TYPE_NONE);
-					// console.log("🚀 ~ oImage:3")
-					// oDrawing.Set_BehindDoc(true);
-					// console.log("🚀 ~ oDrawing22222222222222:", oDrawing)
-					// // oImage.SetWrappingStyle("behind");
-					// console.log("🚀 ~ oImage:", oImage)
-					// oLogicDocument.AddSignatureLine(oDrawing);
-					console.log("🚀 ~ oImage:222222222222222222222")
 					// var oParagraph = this.CreateParagraph();
 					var oParagraph = new AscWord.Paragraph(oLogicDocument);
 					console.log("🚀 ~ oParagraph:", oParagraph)
@@ -9394,19 +9379,11 @@ background-repeat: no-repeat;\
 					// 	return false;
 					// this.WordControl.m_oLogicDocument.GoToBookmark(sId, true);
 					oBookmark[0].GoToBookmark();
-					console.log("🚀 ~ oBookmark:88888888888888888888")
-					// let oRun = new ParaRun(oParagraph, false);
 					let oRun = new AscCommonWord.ParaRun(oParagraph, false);
-					console.log("🚀 ~ oRun:", oRun)
 					oRun.Add_ToContent(0, oDrawing);
-					console.log("🚀 ~ 111111111:")
 					oParagraph.Add_ToContent(oParagraph.Content.length - 1, oRun);
-					console.log("🚀 ~123123123:")
 					oParagraph.CorrectContent(undefined, undefined, true);
-					console.log("🚀 ~888888:")
 					oDrawing.Set_Parent(oRun);
-					console.log("🚀 ~ oParagraph111111111:", oParagraph)
-
 					if (oParagraph.Parent != null) {
 						oParagraph.SetParent(oLogicDocument);
 					}
@@ -9424,35 +9401,40 @@ background-repeat: no-repeat;\
 					var oParagraph1 = oLogicDocument.GetCurrentParagraph(undefined, undefined, { CheckDocContent: true });
 					if (!oParagraph1)
 						return false;
-
 					var oNearestPos = {
 						Paragraph: oParagraph1,
 						ContentPos: oParagraph1.Get_ParaContentPos(false, false)
 					};
-					console.log("🚀 ~ 2222222222222222:")
 					oParagraph1.Check_NearestPos(oNearestPos);
 					oSelectedContent.Insert(oNearestPos);
 					oParagraph1.Clear_NearestPosArray();
 
-					var needDeletePara = oBookmark[0].GetParagraph();
-					console.log("🚀 ~ 3333333333333:")
-					oLogicDocument.RemoveBookmark(sId);
-					console.log("🚀 ~ sId:", sId)
+
+
+
 					// oLogicDocument.InsertContent([oParagraph]);
 					// oParas.Delete();
+					// var needDeleteParaParent = needDeletePara.GetParent();
+					// console.log("🚀 ~ needDeleteParaParent:", needDeleteParaParent)
+					// var nPosInParent = needDeletePara.GetIndex();
+					// console.log("🚀 ~ nPosInParent:", nPosInParent)
+
+					// if (nPosInParent !== - 1) {
+					// 	needDeletePara.PreDelete();
+					// 	needDeleteParaParent.Remove_FromContent(nPosInParent, 1, true);
+					// }
+					// oParagraph.SelectAll(AscWord.Direction.FORWARD);
+					// oLogicDocument.AddBookmark(sId);
+					var needDeletePara = oBookmark[0].GetParagraph();
 					var needDeleteParaParent = needDeletePara.GetParent();
-					console.log("🚀 ~ needDeleteParaParent:", needDeleteParaParent)
 					var nPosInParent = needDeletePara.GetIndex();
-					console.log("🚀 ~ nPosInParent:", nPosInParent)
 
 					if (nPosInParent !== - 1) {
-						needDeletePara.PreDelete();
+						nPosInParent.PreDelete();
 						needDeleteParaParent.Remove_FromContent(nPosInParent, 1, true);
 					}
-					oParagraph.SelectAll(AscWord.Direction.FORWARD);
-					oLogicDocument.AddBookmark(sId);
-
-					// oParagraph.GetRange().AddBookmark(sId);
+					oLogicDocument.RemoveBookmark(sId);
+					oParagraph.GetRange().AddBookmark(sId);
 					oLogicDocument.Recalculate();
 					oLogicDocument.UpdateInterface();
 					oLogicDocument.UpdateSelection();
@@ -9462,7 +9444,6 @@ background-repeat: no-repeat;\
 				}
 			}
 			const image = this.ImageLoader.LoadImage(sUrl, 1);
-			console.log("🚀 ~ image111111111111:", image)
 			if (null != image) {
 				loadImageCallBack(image);
 			}
